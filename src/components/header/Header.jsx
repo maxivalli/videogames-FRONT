6a3821fef0963f84e55cc72d16c22c6f7125a08f
maxivalli <maximilianovalli.sc@gmail.com>
2023-10-03 15:48:29 -0300

    deploy

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from "../../assets/logo.png"
+import Logo from "../../assets/Logo.png"
 import style from './Header.module.css';
 
 export const Header = () => {
